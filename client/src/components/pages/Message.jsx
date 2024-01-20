ee9f1a09500bd259e0a7e476c344852144669643
jadlahirueranga <jadlahirueranga11@gmail.com>
2024-01-20 21:16:32 +0530

    RRD

--- client/src/components/pages/Message.jsx
+++ client/src/components/pages/Message.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate} from "react-router-dom";
-import {useParams } from "react-router";//
+import { useParams } from "react-router-dom";
 import './Message.css';
 
 function Message(auth)
