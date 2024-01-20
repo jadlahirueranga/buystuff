3f3d22f5743ecd0c21c06499a98e415d391235d0
jadlahirueranga <jadlahirueranga11@gmail.com>
2024-01-20 19:12:47 +0530

    useParam fix

--- client/src/components/pages/Message.jsx
+++ client/src/components/pages/Message.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import {useParams } from "react-router";//
 import './Message.css';
 
 function Message(auth)
